dd0a985a9da5998a52768fb51cafce5e3ce99cfb
MaximoPate <patem530@gmail.com>
2026-03-30 08:38:23 -0300

    avance2

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hola, sean bienvenidos a este maravilloso TP");
+});
+
+app.get('/hora', (req, res) => {
+    const now = new Date();
+    const time = now.toLocaleTimeString('es-ES', { hour12: false });
+    res.send(time);
+});
+
+app.get('/fecha-completa', (req, res) => {
+    const now = new Date();
+    const datePart = now.toLocaleDateString('es-ES', { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' });
+    const timePart = now.toLocaleTimeString('es-ES', { hour12: false });
+    res.send(`${datePart}, ${timePart}`);
+});
+
+export default app;
+
